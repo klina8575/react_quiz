0ea900cf177ab07a1afdbe18aa2ac2a9fedea2a6
klina8575 <klina8575@gmail.com>
2024-05-13 13:58:40 +0900

    up

--- ch02/src/App.js
+++ ch02/src/App.js
@@ -3,9 +3,11 @@ import Quiz01 from './Quiz01'
 //import Quiz02 from './Quiz02'
 // import Quiz03 from './Quiz03'
 // import Quiz04 from './Quiz04'
+import Quiz03_sol from './Quiz03_sol'
 
 function App() {
-   return <Quiz01 />
+   // return <Quiz01 />
+   return <Quiz03_sol />
    //return <Quiz02 />
    //return <Quiz03 />
    // return <Quiz04 />
