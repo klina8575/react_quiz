0d7c8abec207270e8c5bd746f930a840ef4dde73
klina8575 <klina8575@gmail.com>
2024-05-14 15:28:06 +0900

    up

--- ch04/src/Quiz02.js
+++ ch04/src/Quiz02.js
@@ -11,17 +11,33 @@ function Quiz02() {
       level: 1,
    })
 
+   const { display1, display2, display3, level } = displayStates
+
+   const evolve = (display, levelVal) => {
+      setDisplayStates({
+         display1: 'none',
+         display2: 'none',
+         display3: 'none',
+      })
+
+      setDisplayStates((prevState) => ({
+         ...prevState,
+         [display]: 'block',
+         level: levelVal,
+      }))
+   }
+
    return (
       <div>
-         <img src={step1} width="150" style={{ display: displayStates.display1 }} />
-         <img src={step2} width="150" style={{ display: displayStates.display2 }} />
-         <img src={step3} width="150" style={{ display: displayStates.display3 }} />
+         <img src={step1} width="150" style={{ display: display1 }} />
+         <img src={step2} width="150" style={{ display: display2 }} />
+         <img src={step3} width="150" style={{ display: display3 }} />
 
          <br />
 
-         <button>이상해씨 진화!</button>
-         <button>이상해꽃 진화!</button>
-         <p>진화 끝!</p>
+         {level === 1 && <button onClick={() => evolve('display2', 2)}>이상해씨 진화!</button>}
+         {level === 2 && <button onClick={() => evolve('display3', 3)}>이상해풀 진화!</button>}
+         {level === 3 && <p>진화 끝!</p>}
       </div>
    )
 }
