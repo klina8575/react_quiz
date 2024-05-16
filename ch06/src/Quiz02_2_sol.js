6a2f16c4c89b95438f2c6c340e63fc8f5a378121
klina8575 <klina8575@gmail.com>
2024-05-16 16:19:52 +0900

    up

--- ch06/src/Quiz02_2_sol.js
+++ ch06/src/Quiz02_2_sol.js
@@ -4,7 +4,7 @@ const Quiz02_2_sol = ({ numbers }) => {
    // useMemo를 사용하여 홀수만 필터링하고 그 합계를 계산합니다.
    const oddSum = useMemo(() => {
       console.log('Calculating odd numbers sum...')
-      return numbers.filter((num) => num % 2 !== 0).reduce((acc, curr) => acc + curr, 0)
+      return numbers.filter((num) => num % 2 !== 0).reduce((acc, curr) => acc + curr)
    }, [numbers])
 
    return (
